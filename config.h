56e6f13c39dfb09538934969fe53a25afda7f34f
Cieran0 <jimmy121212121212121212@gmail.com>
2020-12-07 18:26:02 +0000

    Fixed broken web-browser shortcut

--- config.h
+++ config.h
@@ -58,7 +58,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_purple, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *webbrowser[] = { "firefox", NULL};
+static const char *webbrowser[] = { "firefox-bin", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
