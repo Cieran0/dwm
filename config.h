3e1263b9504339fe412371e9e3b0a56785a47599
Cieran0 <61164049+Cieran0@users.noreply.github.com>
2020-12-08 10:03:17 +0000

    New keybinds

--- config.h
+++ config.h
@@ -59,13 +59,13 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_purple, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *webbrowser[] = { "firefox-bin", NULL};
-static const char *github[] = {"/home/cieran/scripts/github.sh", "NULL"};
-static const char *youtube[] = {"/home/cieran/scripts/youtube.sh", "NULL"};
+static const char *github[] = {"/home/cieran/scripts/github.sh", NULL};
+static const char *youtube[] = {"/home/cieran/scripts/youtube.sh", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_t,      spawn,          {.v = termcmd } },
 	{ MODKEY,             		XK_w, 	   spawn,          {.v = webbrowser } },
 	{ MODKEY,                       XK_g,      spawn,          {.v = github } },
 	{ MODKEY,                       XK_y,      spawn,          {.v = youtube } },
